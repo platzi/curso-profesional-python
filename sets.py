af2cecbe1710ada94b126a48a207fe287573c8fe
Facundo García Martoni <thunderxforce@gmail.com>
2021-06-08 23:46:57 -0300

    sets finished

---
+++ sets.py
@@ -0,0 +1,15 @@
+def remove_duplicates(some_list):
+    without_duplicates = []
+    for element in some_list:
+        if element not in without_duplicates:
+            without_duplicates.append(element)
+    return without_duplicates
+
+
+def run():
+    random_list = [1, 2, 2, 2, 3, "Platzi", "Platzi", True, 4.6, False]
+    print(remove_duplicates(random_list))
+
+
+if __name__ == '__main__':
+    run()
